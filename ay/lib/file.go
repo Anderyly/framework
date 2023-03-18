501eac648881527d18d237e73251672142c1d2d8
anderyly <admin@aaayun.cc>
2023-03-18 09:18:04 +0800

    edit lib file

--- ay/lib/file.go
+++ ay/lib/file.go
@@ -8,13 +8,16 @@
 
 package lib
 
-import "os"
+import (
+	"io"
+	"os"
+)
 
 var _ File = (*file)(nil)
 
 type File interface {
-	IsFile(filename string) bool
-	Upload()
+	IsFile(filename string) bool                                // 判断文件是否存在
+	Write(filename string, data []byte, perm os.FileMode) error // 文件写入
 }
 
 type file struct {
@@ -32,6 +35,17 @@ func (con *file) IsFile(filename string) bool {
 	return true
 }
 
-func (con *file) Upload() {
-
+func (con *file) Write(filename string, data []byte, perm os.FileMode) error {
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, perm)
+	if err != nil {
+		return err
+	}
+	n, err := f.Write(data)
+	if err == nil && n < len(data) {
+		err = io.ErrShortWrite
+	}
+	if err1 := f.Close(); err == nil {
+		err = err1
+	}
+	return err
 }
